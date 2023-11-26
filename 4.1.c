a245237fc6b73b199713afa6d2047dabcad79244
Piotr Lachowicz <120327639+MikyTuberr@users.noreply.github.com>
2023-11-26 17:54:48 +0100

    Update and rename main.c to 4.1.c

--- 4.1.c
+++ 4.1.c
@@ -8,6 +8,6 @@ int main()
 	printf("\nProsze podac trzy liczby calkowite ze znakiem: ");
 	scanf_s("%d %d %d %d", &x, &y, &z, &d, 32);
 	wynik = szukaj_max(x, y, z, d);
-	printf("\nSposród podanych liczb %d, %d, %d, %d, liczba %d jest najwieksza\n", x, y, z, d, wynik);
+	printf("\nSposrÃ³d podanych liczb %d, %d, %d, %d, liczba %d jest najwieksza\n", x, y, z, d, wynik);
 	return 0;
 }
